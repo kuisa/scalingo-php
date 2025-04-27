876bf38c28a9152b574b9aff21945758407c812e
kuisa <32757027+kuisa@users.noreply.github.com>
2025-04-27 21:27:46 +0800

    Add files via upload

---
+++ data/get1.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting(0);
+header('Content-Type: text/json;charset=UTF-8');
+$data = $_GET["data"];
+$data = base64_decode($data);
+$data = trim($data);
+$data = openssl_decrypt($data,'AES-128-ECB',"twcdncreate22022",0);
+$url = explode("&",$data)[0];
+$header = json_decode(explode("&",$data)[1]);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+$data = curl_exec($ch);
+curl_close($ch);
+print_r($data);
+?>
---
+++ data/get3.php
@@ -0,0 +1,16 @@
+<?php
+error_reporting(0);
+header('Content-Type: text/json;charset=UTF-8');
+$data = $_GET["data"];
+$data = base64_decode($data);
+$data = trim($data);
+$url = openssl_decrypt($data,'AES-128-ECB',"twcdncreate22022",0);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+$data = curl_exec($ch);
+curl_close($ch);
+print_r($data);
+?>
